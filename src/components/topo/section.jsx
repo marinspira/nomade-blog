720770d246f4271ae936c05dc73a71cd1964b19a
Maria Eduarda Ferreira <maria.ferreira@feedz.com.br>
2024-02-12 22:06:13 -0300

    Ajusta img de map

--- src/components/topo/section.jsx
+++ src/components/topo/section.jsx
@@ -2,12 +2,35 @@ import { PostsContext } from "@/context/allPosts";
 import styles from './section.module.css'
 import { useContext, useEffect, useState } from "react";
 import { extractFirstImage } from "@/utils/extractImg";
-import map from '@/assets/map4.png'
+import map from '@/assets/map5.svg'
 
 function Topo({ params }) {
     const { posts } = useContext(PostsContext);
     
     const [firstImages, setFirstImages] = useState([]);
+
+    const [dragging, setDragging] = useState(false);
+
+    const handleDragStart = (e) => {
+        e.preventDefault();
+        setDragging(true);
+    };
+
+    const handleDragEnd = () => {
+        setDragging(false);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setDragging(false);
+        const dropX = e.clientX;
+        const dropY = e.clientY;
+        // Aqui você pode adicionar a lógica para manipular a posição da imagem
+    };
 
     useEffect(() => {
         if (posts.length > 0) {
@@ -19,8 +42,6 @@ function Topo({ params }) {
         }
     }, [posts]);
 
-    console.log(posts[0])
-
     return (
         <div className={styles.row}>
             <div className={styles.column}>
@@ -28,16 +49,24 @@ function Topo({ params }) {
                 <h1>{posts.title}</h1>
             </div>
             <div className={styles.column2}>
-                <div className={styles.map}>
-                    <img src={map.src} alt="map"/>
+                <div className={styles.map}
+                    draggable
+                    onDragStart={handleDragStart}
+                    onDragEnd={handleDragEnd}
+                    onDragOver={handleDragOver}
+                    onDrop={handleDrop}
+                >
+                    <img src={map.src} alt="map" />
                 </div>
                 <div className={styles.posts}>
                     {posts.map((post, index) => (
-                        <a className={styles.post} href={`/post/${post.id}`} key={post.id}>
-                            <label>{post.labels}</label>
-                            <img src={firstImages[index]} alt={post.title}/>
-                            <h2>{post.title}</h2>
-                        </a>
+                        <div className={styles.post}>
+                            <a href={`/post/${post.id}`} key={post.id}>
+                                <label>{post.labels}</label>
+                                <img src={firstImages[index]} alt={post.title}/>
+                                <h2>{post.title}</h2>
+                            </a>
+                        </div>
                     ))}
                 </div>
             </div>
